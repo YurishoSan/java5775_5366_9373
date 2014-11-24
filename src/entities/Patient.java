5b53ff013b5aafd8d0d7f808c80c5fdddd36d655
YurishoSan <yurisho@gmail.com>
2014-11-25 00:06:14 +0200

    Remove Human
    -Remove Human class
    *Change Patient and Doctor classes to not extend human, and instead
    implement it's attributes inside themselves
    
    *update Javadoc

--- src/entities/Patient.java
+++ src/entities/Patient.java
@@ -9,6 +9,10 @@ import java.util.Date;
 
 
 
+
+
+
+
 import enums.Gender;
 import enums.ServiceClass;
 
@@ -20,9 +24,15 @@ import enums.ServiceClass;
  * 
  * @see enums.ServiceClass
  */
-public class Patient extends Human
+public class Patient
 {
 	//region attributes
+	private long HumanID;
+	private String HumanFirstName;
+	private String HumanLastName;
+	private Gender HumanGender;
+	private Date HumanDoB; //Date of birth
+	private String EmailAdress;
 	private ServiceClass PatientServiceClass; // level of service the company provides to the patient
 	private String PatientPhoneNumber;	
 	//endregion
@@ -48,9 +58,15 @@ public class Patient extends Human
 	 */
 	public Patient(long humanID, String humanFirstName, String humanLastName,
 			Gender humanGender, Date humanDoB, String emailAdress,
-			ServiceClass patientServiceClass, String patientPhoneNumber) {
-		super(humanID, humanFirstName, humanLastName, humanGender, humanDoB,
-				emailAdress);
+			ServiceClass patientServiceClass, String patientPhoneNumber)
+	{
+		super();
+		HumanID = humanID;
+		HumanFirstName = humanFirstName;
+		HumanLastName = humanLastName;
+		HumanGender = humanGender;
+		HumanDoB = humanDoB;
+		EmailAdress = emailAdress;
 		PatientServiceClass = patientServiceClass;
 		PatientPhoneNumber = patientPhoneNumber;
 	}
@@ -58,12 +74,110 @@ public class Patient extends Human
 	//endregion
 
 	//region getters/setters
+	
+	
 	/**
 	 * @return the patientServiceClass
 	 */
 	public ServiceClass getPatientServiceClass()
 	{
 		return PatientServiceClass;
+	}
+
+	/**
+	 * @return the humanID
+	 */
+	public long getHumanID()
+	{
+		return HumanID;
+	}
+
+	/**
+	 * @param humanID the humanID to set
+	 */
+	public void setHumanID(long humanID)
+	{
+		HumanID = humanID;
+	}
+
+	/**
+	 * @return the humanFirstName
+	 */
+	public String getHumanFirstName()
+	{
+		return HumanFirstName;
+	}
+
+	/**
+	 * @param humanFirstName the humanFirstName to set
+	 */
+	public void setHumanFirstName(String humanFirstName)
+	{
+		HumanFirstName = humanFirstName;
+	}
+
+	/**
+	 * @return the humanLastName
+	 */
+	public String getHumanLastName()
+	{
+		return HumanLastName;
+	}
+
+	/**
+	 * @param humanLastName the humanLastName to set
+	 */
+	public void setHumanLastName(String humanLastName)
+	{
+		HumanLastName = humanLastName;
+	}
+
+	/**
+	 * @return the humanGender
+	 */
+	public Gender getHumanGender()
+	{
+		return HumanGender;
+	}
+
+	/**
+	 * @param humanGender the humanGender to set
+	 */
+	public void setHumanGender(Gender humanGender)
+	{
+		HumanGender = humanGender;
+	}
+
+	/**
+	 * @return the humanDoB
+	 */
+	public Date getHumanDoB()
+	{
+		return HumanDoB;
+	}
+
+	/**
+	 * @param humanDoB the humanDoB to set
+	 */
+	public void setHumanDoB(Date humanDoB)
+	{
+		HumanDoB = humanDoB;
+	}
+
+	/**
+	 * @return the emailAdress
+	 */
+	public String getEmailAdress()
+	{
+		return EmailAdress;
+	}
+
+	/**
+	 * @param emailAdress the emailAdress to set
+	 */
+	public void setEmailAdress(String emailAdress)
+	{
+		EmailAdress = emailAdress;
 	}
 
 	/**
@@ -92,17 +206,45 @@ public class Patient extends Human
 	//endregion
 
 	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (HumanID ^ (HumanID >>> 32));
+		return result;
+	}
+
+	//region overrideMethods
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Patient other = (Patient) obj;
+		if (HumanID != other.HumanID)
+			return false;
+		return true;
+	}
+
+	/* (non-Javadoc)
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
 	public String toString()
 	{
 		StringBuilder builder = new StringBuilder();
-		builder.append("Patient [PatientServiceClass=");
-		builder.append(PatientServiceClass);
-		builder.append(", PatientPhoneNumber=");
-		builder.append(PatientPhoneNumber);
-		builder.append(", HumanID=");
+		builder.append("Patient [HumanID=");
 		builder.append(HumanID);
 		builder.append(", HumanFirstName=");
 		builder.append(HumanFirstName);
@@ -112,7 +254,15 @@ public class Patient extends Human
 		builder.append(HumanGender);
 		builder.append(", HumanDoB=");
 		builder.append(HumanDoB);
+		builder.append(", EmailAdress=");
+		builder.append(EmailAdress);
+		builder.append(", PatientServiceClass=");
+		builder.append(PatientServiceClass);
+		builder.append(", PatientPhoneNumber=");
+		builder.append(PatientPhoneNumber);
 		builder.append("]");
 		return builder.toString();
 	}
+
+	//endregion
 }
