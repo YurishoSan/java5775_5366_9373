e2e61406bbc63699708050d9221d220f69c7b2a9
tzichtig <c.shalom21@gmail.com>
2014-12-25 19:40:34 +0200

    addTreatmentActivity

--- src/control/TreatmentActivity.java
+++ src/control/TreatmentActivity.java
@@ -1,11 +1,19 @@
 package control;
 
+import model.backend.BackendFactory;
+
 import com.example.java5775_5366_9373.R;
 
+import entities.Treatment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View.OnClickListener;
+import android.view.View;
 
 public class TreatmentActivity extends ActionBarActivity {
 
@@ -13,24 +21,40 @@ public class TreatmentActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_treatment);
+		Button button = (Button) findViewById(R.id.button2);
+	
+		button.setOnClickListener(new OnClickListener()
+		{
+			public void onClick (View v)
+			{
+				try
+				{
+			    EditText patieId =(EditText)findViewById(R.id.patieIdText);
+			    EditText addres =(EditText)findViewById(R.id.addresText);
+			    EditText summary =(EditText)findViewById(R.id.sumaryText);
+			    
+			    Treatment NewTreat =new Treatment();
+			    NewTreat.setTreatmentPatientID(Long.parseLong(patieId.getText().toString()));
+                NewTreat.setTreatmentLocation(addres.getText().toString());
+                NewTreat.setTreatmentSummary(summary.getText().toString());
+                BackendFactory.getInstance(getApplicationContext()).addTreatment(NewTreat);
+					
+				}
+				catch(Exception exp)
+				{
+					
+				}
+				
+			}
+		});
+		
+		
+		
+		
+		
+		
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.treatment, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+	
+	
 }
